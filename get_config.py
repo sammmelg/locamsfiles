b576c5e56e9e86838a8b03287f2837d118ee3aa5
sammmelg <93548331+sammmelg@users.noreply.github.com>
2022-03-29 08:11:10 -0700

    Add files via upload

--- get_config.py
+++ get_config.py
@@ -3,17 +3,17 @@ import os
 import argparse
 
 def main():
-    print('Getting config file, please wait')
-    conf_file = get_config()
-    print(conf_file.latitude)
+    get_config()
+
 
 def get_config():
     arg_parser = argparse.ArgumentParser(description=""" Tests if device opened for capture.""")
-    arg_parser.add_argument('-c', '--config', nargs=1, metavar='CONFIG_PATH', type=str, help="Path to a config file which will be used instead of the default one")
+    arg_parser.add_argument('-c', '--config', nargs=1, metavar='CONFIG_PATH', type=str,
+                            help="Path to a config file which will be used instead of the default one")
     cml_args = arg_parser.parse_args()
     config = cr.loadConfigFromDirectory(cml_args.config, os.path.abspath('.'))
-
-    return config
+    print(config.latitude)
+    return config.latitude
 
 if __name__ == '__main__':
     main()
