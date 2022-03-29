25ce13bfe99c75fce9f249317c276ba30cb208cd
sammmelg <93548331+sammmelg@users.noreply.github.com>
2022-03-29 07:57:07 -0700

    Add files via upload

--- get_config.py
+++ get_config.py
@@ -2,6 +2,9 @@ import RMS.ConfigReader as cr
 import os
 import argparse
 
+def main():
+    get_config()
+
 def get_config():
     arg_parser = argparse.ArgumentParser(description=""" Tests if device opened for capture.""")
     arg_parser.add_argument('-c', '--config', nargs=1, metavar='CONFIG_PATH', type=str,
@@ -10,3 +13,6 @@ def get_config():
     config = cr.loadConfigFromDirectory(cml_args.config, os.path.abspath('.'))
 
     print(config.latitude)
+
+if __name__ == '__main__':
+    main()
