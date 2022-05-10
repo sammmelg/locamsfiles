78958605cabf3ca59f078e80e0d73b2af45ce314
sammmelg <93548331+sammmelg@users.noreply.github.com>
2022-05-10 07:50:09 -0700

    Add files via upload

--- SlackTest.py
+++ SlackTest.py
@@ -1,6 +1,6 @@
 import os
 import time
-import slack
+import slack_sdk
 import datetime
 
 stat_id = str(os.uname()[1])
@@ -12,5 +12,5 @@ current_time = now.strftime('%H:%M:%S on %m/%d/%Y')
 
 # Send a slack alert to LOCAMS alerts slack channel
 slack_token = os.environ.get('SLACK_TOKEN')
-client = slack.WebClient(token=slack_token)
+client = slack_sdk.WebClient(token=slack_token)
 client.chat_postMessage(channel='alert-system', text=f'This is only a test from {stat_id} at {current_time}!')
